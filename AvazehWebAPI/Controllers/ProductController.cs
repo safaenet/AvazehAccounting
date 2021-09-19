5c88c8956326a1f50135de2c59af925ad14911fc
safaenet <safa.dh@gmail.com>
2021-09-19 13:17:03 +0430

    Working

---
+++ AvazehWebAPI/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using DataLibraryCore.DataAccess.CollectionManagers;
+using DataLibraryCore.DataAccess.Interfaces;
+using DataLibraryCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AvazehWebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        public ProductController(IProductCollectionManager manager)
+        {
+            Manager = manager;
+        }
+        private readonly IProductCollectionManager Manager;
+
+        [HttpGet]
+        public IProductCollectionManager Get()
+        {
+            Manager.LoadFirstPage();
+            return Manager;
+        }
+
+        //GET ./Product?Id=1,SearchText=sometext
+        [HttpGet("{Id},{SearchText}")]
+        public IProductCollectionManager Get(int Id = 1, string SearchText = "")
+        {
+            Manager.GenerateWhereClause(SearchText);
+            Manager.GotoPage(Id);
+            return Manager;
+        }
+    }
+}
