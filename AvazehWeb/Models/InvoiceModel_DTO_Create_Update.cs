6bd913b8db66280511559a8834ef86b27b2e6fd8
safaenet <safa.dh@gmail.com>
2021-09-26 10:55:33 +0330

    I

---
+++ AvazehWeb/Models/InvoiceModel_DTO_Create_Update.cs
@@ -0,0 +1,22 @@
+﻿using DataLibraryCore.Models;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AvazehWeb.Models
+{
+    public class InvoiceModel_DTO_Create_Update
+    {
+        public int CustomerId { get; set; }
+
+        [Display(Name = "Discount Type")]
+        public DiscountTypes DiscountType { get; set; }
+
+        [Display(Name = "Discount Value")]
+        [Range(0, double.MaxValue, ErrorMessage = "Field must be greater than Zero")]
+        public double DiscountValue { get; set; }
+
+        [Display(Name = "Description")]
+        public string Descriptions { get; set; }
+        public InvoiceLifeStatus LifeStatus { get; set; }
+    }
+}
---
+++ AvazehWeb/Models/InvoiceModel_DTO_Read.cs
@@ -0,0 +1,15 @@
+﻿using DataLibraryCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvazehWeb.Models
+{
+    public class InvoiceModel_DTO_Read
+    {
+        public InvoiceModel Invoice { get; set; }
+        public double CustomerTotalBalance { get; set; }
+    }
+}
