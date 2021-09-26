ddda1df37f2d5161f961b25d3906b36490c557ac
safaenet <safa.dh@gmail.com>
2021-09-26 20:37:43 +0330

    h

--- AvazehWeb/Controllers/ApiCustomerController.cs
+++ AvazehWeb/Controllers/ApiCustomerController.cs
@@ -13,30 +13,28 @@ namespace AvazehWebAPI.Controllers
 {
     [ApiController]
     [Route("api/v1/[controller]")]
-    public class CustomerController<TDal, TDto, TCollection, TManager> : ControllerBase
-        where TDal : CustomerModel where TDto : CustomerModel_DTO_Create_Update
-        where TCollection : ItemsCollection_DTO<TDal> where TManager : ICollectionManager<TDal, IProcessor<TDal>>
+    public class CustomerController : ControllerBase
     {
-        public CustomerController(TManager manager)
+        public CustomerController(ICollectionManager<CustomerModel, IProcessor<CustomerModel>> manager)
         {
             Manager = manager;
         }
 
-        private readonly TManager Manager;
+        private readonly ICollectionManager<CustomerModel, IProcessor<CustomerModel>> Manager;
 
         //GET /Customer?Id=1&SearchText=sometext
         [HttpGet]
-        public async Task<ActionResult<TCollection>> GetItemsAsync(int Page = 1, string SearchText = "", string OrderBy = "FirstName", OrderType orderType = OrderType.ASC, int PageSize = 50)
+        public async Task<ActionResult<ItemsCollection_DTO<CustomerModel>>> GetItemsAsync(int Page = 1, string SearchText = "", string OrderBy = "FirstName", OrderType orderType = OrderType.ASC, int PageSize = 50)
         {
             Manager.GenerateWhereClause(SearchText, OrderBy, orderType);
             Manager.PageSize = PageSize;
             await Manager.GotoPageAsync(Page);
             if (Manager.Items == null || Manager.Items.Count == 0) return NotFound("List is empty");
-            return Manager.AsDto() as TCollection;
+            return Manager.AsDto();
         }
 
         [HttpGet("{Id}")]
-        public async Task<ActionResult<TDal>> GetItemAsync(int Id)
+        public async Task<ActionResult<CustomerModel>> GetItemAsync(int Id)
         {
             var item = await Manager.Processor.LoadSingleItemAsync(Id);
             if (item is null) return NotFound("Couldn't find specific Item");
@@ -44,23 +42,23 @@ namespace AvazehWebAPI.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult<TDal>> CreateItemAsync(TDto model)
+        public async Task<ActionResult<CustomerModel>> CreateItemAsync(CustomerModel_DTO_Create_Update model)
         {
             var newItem = model.AsDaL();
-            if (!Manager.Processor.ValidateItem(newItem as TDal).IsValid) return BadRequest(0);
-            await Manager.Processor.CreateItemAsync(newItem as TDal);
-            return newItem as TDal;
+            if (!Manager.Processor.ValidateItem(newItem as CustomerModel).IsValid) return BadRequest(0);
+            await Manager.Processor.CreateItemAsync(newItem as CustomerModel);
+            return newItem as CustomerModel;
         }
 
         [HttpPut("{Id}")]
-        public async Task<ActionResult<TDal>> UpdateItemAsync(int Id, TDto model)
+        public async Task<ActionResult<CustomerModel>> UpdateItemAsync(int Id, CustomerModel_DTO_Create_Update model)
         {
             if (model is null) return BadRequest("Model is not valid");
             var updatedModel = model.AsDaL();
-            if (!Manager.Processor.ValidateItem(updatedModel as TDal).IsValid) return BadRequest("Model is not valid");
+            if (!Manager.Processor.ValidateItem(updatedModel as CustomerModel).IsValid) return BadRequest("Model is not valid");
             updatedModel.Id = Id;
-            if (await Manager.Processor.UpdateItemAsync(updatedModel as TDal) == 0) return NotFound();
-            return updatedModel as TDal;
+            if (await Manager.Processor.UpdateItemAsync(updatedModel as CustomerModel) == 0) return NotFound();
+            return updatedModel as CustomerModel;
         }
 
         [HttpDelete]
