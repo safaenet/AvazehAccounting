87c2f0da437e25c4546b1e4c61fb731c3303b7f9
safaenet <safa.dh@gmail.com>
2022-04-17 09:34:46 +0430

    2

--- AvazehApiClient/DataAccess/Interfaces/ICollectionManager.cs
+++ AvazehApiClient/DataAccess/Interfaces/ICollectionManager.cs
@@ -6,37 +6,9 @@ using System.Threading.Tasks;
 
 namespace AvazehApiClient.DataAccess.Interfaces
 {
-    public interface ICollectionManager<T>
+    public interface ICollectionManager<T> : ICollectionManagerBase<T>
     {
-        IApiProcessor ApiProcessor { get; init; }
-        int CurrentPage { get; }
         ObservableCollection<T> Items { get; set; }
-        int? MaxID { get; }
-        int? MinID { get; }
-        int PagesCount { get; }
-        int PageSize { get; set; }
-        string SearchValue { get; set; }
-        string QueryOrderBy { get; set; }
-        OrderType QueryOrderType { get; set; }
-
-        event EventHandler PageLoading;
-        event EventHandler PageLoaded;
-        event EventHandler FirstPageLoaded;
-        event EventHandler NextPageLoaded;
-        event EventHandler NextPageLoading;
-        event EventHandler PreviousPageLoaded;
-        event EventHandler PreviousPageLoading;
-
-        Task<T> CreateItemAsync(T item);
-        Task<bool> DeleteItemAsync(int Id);
         T GetItemFromCollectionById(int Id);
-        Task<T> GetItemById(int Id);
-        Task<int> GotoPageAsync(int PageNumber, bool Refresh);
-        Task<int> RefreshPage();
-        Task<int> LoadFirstPageAsync();
-        Task<int> LoadNextPageAsync();
-        Task<int> LoadPreviousPageAsync();
-        Task<T> UpdateItemAsync(T item);
-        ValidationResult ValidateItem(T item);
     }
 }
--- AvazehApiClient/DataAccess/Interfaces/IInvoiceCollectionManager.cs
+++ AvazehApiClient/DataAccess/Interfaces/IInvoiceCollectionManager.cs
@@ -9,40 +9,12 @@ using System.Threading.Tasks;
 
 namespace AvazehApiClient.DataAccess.Interfaces
 {
-    public interface IInvoiceCollectionManager
+    public interface IInvoiceCollectionManager : ICollectionManagerBase<InvoiceModel>
     {
-        IApiProcessor ApiProcessor { get; init; }
-        int CurrentPage { get; }
         ObservableCollection<InvoiceListModel> Items { get; set; }
-        int? MaxID { get; }
-        int? MinID { get; }
-        int PagesCount { get; }
-        int PageSize { get; set; }
-        string SearchValue { get; set; }
-        string QueryOrderBy { get; set; }
-        OrderType QueryOrderType { get; set; }
         InvoiceLifeStatus? LifeStatus { get; set; }
         InvoiceFinancialStatus? FinStatus { get; set; }
-
-        event EventHandler FirstPageLoaded;
-        event EventHandler NextPageLoaded;
-        event EventHandler NextPageLoading;
-        event EventHandler PageLoaded;
-        event EventHandler PageLoading;
-        event EventHandler PreviousPageLoaded;
-        event EventHandler PreviousPageLoading;
-
-        Task<InvoiceModel> CreateItemAsync(InvoiceModel item);
-        Task<bool> DeleteItemAsync(int Id);
-        Task<InvoiceModel> GetItemById(int Id);
         InvoiceListModel GetItemFromCollectionById(int Id);
-        Task<int> GotoPageAsync(int PageNumber, bool Refresh);
-        Task<int> RefreshPage();
-        Task<int> LoadFirstPageAsync();
-        Task<int> LoadNextPageAsync();
-        Task<int> LoadPreviousPageAsync();
-        Task<InvoiceModel> UpdateItemAsync(InvoiceModel item);
-        ValidationResult ValidateItem(InvoiceModel item);
         Task<List<ProductNamesForComboBox>> LoadProductItems(string SearchText = null);
         Task<double> GetCustomerTotalBalanceById(int CustomerId, int InvoiceId = 0);
     }
