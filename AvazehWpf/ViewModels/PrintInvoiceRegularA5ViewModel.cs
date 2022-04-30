e6f3e4170df32ebff0e5516724762a9a25b89095
safaenet <safa.dh@gmail.com>
2022-04-30 18:24:07 +0430

    6

--- AvazehWpf/ViewModels/PrintInvoiceRegularA5ViewModel.cs
+++ AvazehWpf/ViewModels/PrintInvoiceRegularA5ViewModel.cs
@@ -17,10 +17,12 @@ using System.Windows.Input;
 using System.Windows.Data;
 using System.Collections.ObjectModel;
 using System.Windows.Threading;
+using AvazehWpf.Views;
+using System.Windows.Documents;
 
 namespace AvazehWpf.ViewModels
 {
-    public class PrintInvoiceRegularA5ViewModel : ViewAware
+    public class PrintInvoiceRegularA5ViewModel : IViewAware
     {
         public PrintInvoiceRegularA5ViewModel(InvoiceModel invoice)
         {
@@ -28,11 +30,44 @@ namespace AvazehWpf.ViewModels
         }
 
         private InvoiceModel _invoice;
+        private PrintInvoiceRegularA5View _view;
+
+        public event EventHandler<ViewAttachedEventArgs> ViewAttached;
 
         public InvoiceModel Invoice
         {
             get { return _invoice; }
-            set { _invoice = value; NotifyOfPropertyChange(() => Invoice); }
+            set { _invoice = value; /*NotifyOfPropertyChange(() => Invoice);*/ }
+        }
+
+        public void PrintDoc()
+        {
+            //var pd=new PrintDialog();
+            //if (pd.ShowDialog() == true)
+            //{
+            //    IDocumentPaginatorSource idp = _view.FlowDocX;
+            //    pd.PrintDocument(idp.DocumentPaginator, "Avazeh");
+            //}
+            var paginator = new ProgramPaginator(_view);
+            var dlg = new PrintDialog();
+            if ((bool)dlg.ShowDialog())
+            {
+                paginator.PageSize = new Size(dlg.PrintableAreaWidth, dlg.PrintableAreaHeight);
+                dlg.PrintDocument(paginator, "Program");
+            }
+        }
+
+        public void AttachView(object view, object context = null)
+        {
+            _view = view as PrintInvoiceRegularA5View;
+            if (ViewAttached != null)
+                ViewAttached(this,
+                new ViewAttachedEventArgs() { Context = context, View = view });
+        }
+
+        public object GetView(object context = null)
+        {
+            return _view;
         }
     }
 }
