ae9478497534a653d890573866bd7a7a0cd83603
Safa Seed <safa.dh@gmail.com>
2022-09-13 23:57:52 +0430

    22

--- AvazehControlLibraryWpf/PersianDatePicker.xaml.cs
+++ AvazehControlLibraryWpf/PersianDatePicker.xaml.cs
@@ -30,7 +30,46 @@ namespace AvazehUserControlLibraryWpf
             Months = new();
             Days = new();
             for (int i = 1; i <= 31; i++) Days.Add(i);
-            FillYearsAndMonths();
+            FillYears();
+            //PersianDate = PC.GetPersianDate();
+        }
+
+        private void FillMonths()
+        {
+            if (MonthDisplayType == MonthType.Full)
+            {
+                Months.Add(new(1, "(فروردین (1"));
+                Months.Add(new(2, "(اردیبهشت (2"));
+                Months.Add(new(3, "(خرداد (3"));
+                Months.Add(new(4, "(تیر (4"));
+                Months.Add(new(5, "(مرداد (5"));
+                Months.Add(new(6, "(شهریور (6"));
+                Months.Add(new(7, "(مهر (7"));
+                Months.Add(new(8, "(آبان (8"));
+                Months.Add(new(9, "(آذر (9"));
+                Months.Add(new(10, "(دی (10"));
+                Months.Add(new(11, "(بهمن (11"));
+                Months.Add(new(12, "(اسفند (12"));
+            }
+            else if (MonthDisplayType == MonthType.Letters)
+            {
+                Months.Add(new(1, "فروردین"));
+                Months.Add(new(2, "اردیبهشت "));
+                Months.Add(new(3, "خرداد "));
+                Months.Add(new(4, "تیر"));
+                Months.Add(new(5, "مرداد"));
+                Months.Add(new(6, "شهریور"));
+                Months.Add(new(7, "مهر"));
+                Months.Add(new(8, "آبان"));
+                Months.Add(new(9, "آذر"));
+                Months.Add(new(10, "دی"));
+                Months.Add(new(11, "بهمن"));
+                Months.Add(new(12, "اسفند"));
+            }
+            else if (MonthDisplayType == MonthType.Numbers)
+            {
+                for (int i = 1; i <= 12; i++) Months.Add(new(i, i.ToString()));
+            }
         }
 
         private PersianCalendar PC;
@@ -48,12 +87,91 @@ namespace AvazehUserControlLibraryWpf
         }
 
         public static DependencyProperty PersianDateProperty =
-            DependencyProperty.Register("PersianDate", typeof(string), typeof(PersianDatePicker), new PropertyMetadata("1395/01/01", OnMainDateChanged));
+            DependencyProperty.Register("PersianDate", typeof(string), typeof(PersianDatePicker), new PropertyMetadata("1395/02/28", OnMainDateChanged));
+
+
+        public int YearWidth
+        {
+            get { return (int)GetValue(YearWidthProperty); }
+            set { SetValue(YearWidthProperty, value); }
+        }
+        public int DayWidth
+        {
+            get { return (int)GetValue(DayWidthProperty); }
+            set { SetValue(DayWidthProperty, value); }
+        }
+        public int MonthFontSize
+        {
+            get { return (int)GetValue(MonthFontSizeProperty); }
+            set { SetValue(MonthFontSizeProperty, value); }
+        }
+        public MonthType MonthDisplayType
+        {
+            get { return (MonthType)GetValue(MonthDisplayTypeProperty); }
+            set { SetValue(MonthDisplayTypeProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for YearWidth.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty YearWidthProperty =
+            DependencyProperty.Register("YearWidth", typeof(int), typeof(PersianDatePicker), new PropertyMetadata(55));
+
+        // Using a DependencyProperty as the backing store for YearWidth.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty DayWidthProperty =
+            DependencyProperty.Register("DayWidth", typeof(int), typeof(PersianDatePicker), new PropertyMetadata(41));
+
+        // Using a DependencyProperty as the backing store for YearWidth.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MonthFontSizeProperty =
+            DependencyProperty.Register("MonthFontSize", typeof(int), typeof(PersianDatePicker), new PropertyMetadata(12));
+
+        // Using a DependencyProperty as the backing store for YearWidth.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MonthDisplayTypeProperty =
+            DependencyProperty.Register("MonthDisplayType", typeof(MonthType), typeof(PersianDatePicker), new PropertyMetadata(MonthType.None, MonthTypeChanged));
+
+        private static void MonthTypeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var pdp = d as PersianDatePicker;
+            if (e.NewValue is null || pdp is null) return;
+            pdp.Months.Clear();
+            if (pdp.MonthDisplayType == MonthType.Full)
+            {
+                pdp.Months.Add(new(1, "(فروردین (1"));
+                pdp.Months.Add(new(2, "(اردیبهشت (2"));
+                pdp.Months.Add(new(3, "(خرداد (3"));
+                pdp.Months.Add(new(4, "(تیر (4"));
+                pdp.Months.Add(new(5, "(مرداد (5"));
+                pdp.Months.Add(new(6, "(شهریور (6"));
+                pdp.Months.Add(new(7, "(مهر (7"));
+                pdp.Months.Add(new(8, "(آبان (8"));
+                pdp.Months.Add(new(9, "(آذر (9"));
+                pdp.Months.Add(new(10, "(دی (10"));
+                pdp.Months.Add(new(11, "(بهمن (11"));
+                pdp.Months.Add(new(12, "(اسفند (12"));
+            }
+            else if (pdp.MonthDisplayType == MonthType.Letters)
+            {
+                pdp.Months.Add(new(1, "فروردین"));
+                pdp.Months.Add(new(2, "اردیبهشت "));
+                pdp.Months.Add(new(3, "خرداد "));
+                pdp.Months.Add(new(4, "تیر"));
+                pdp.Months.Add(new(5, "مرداد"));
+                pdp.Months.Add(new(6, "شهریور"));
+                pdp.Months.Add(new(7, "مهر"));
+                pdp.Months.Add(new(8, "آبان"));
+                pdp.Months.Add(new(9, "آذر"));
+                pdp.Months.Add(new(10, "دی"));
+                pdp.Months.Add(new(11, "بهمن"));
+                pdp.Months.Add(new(12, "اسفند"));
+            }
+            else if (pdp.MonthDisplayType == MonthType.Numbers)
+            {
+                for (int i = 1; i <= 12; i++) pdp.Months.Add(new(i, i.ToString()));
+            }
+        }
 
         private static void OnMainDateChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             var pdp = d as PersianDatePicker;
-            if (e.NewValue is null) return;
+            if (e.NewValue is null || pdp is null) return;
             var subs = ((string)e.NewValue).Split('/');
             if (subs.Length != 3) throw new InvalidCastException();
             if (!int.TryParse(subs[2], out var day)) throw new InvalidCastException();
@@ -102,23 +220,9 @@ namespace AvazehUserControlLibraryWpf
             if (pdp is not null && e.Property == DayProperty) pdp.PersianDate = string.Format("{0:0000}/{1:00}/{2:00}", pdp.Year, pdp.Month, e.NewValue);
         }
 
-        private void FillYearsAndMonths()
+        private void FillYears()
         {
             for (int i = 1390; i <= 1450; i++) Years.Add(i);
-
-            Months.Add(new(1, "(فروردین (1"));
-            Months.Add(new(2, "(اردیبهشت (2"));
-            Months.Add(new(3, "(خرداد (3"));
-            Months.Add(new(4, "(تیر (4"));
-            Months.Add(new(5, "(مرداد (5"));
-            Months.Add(new(6, "(شهریور (6"));
-            Months.Add(new(7, "(مهر (7"));
-            Months.Add(new(8, "(آبان (8"));
-            Months.Add(new(9, "(آذر (9"));
-            Months.Add(new(10, "(دی (10"));
-            Months.Add(new(11, "(بهمن (11"));
-            Months.Add(new(12, "(اسفند (12"));
-
         }
 
         private void FillDays()
@@ -128,6 +232,14 @@ namespace AvazehUserControlLibraryWpf
             if (Month == 12 && !PC.IsLeapYear(Year)) Days.Remove(30);
             if (Month >= 7) Days.Remove(31);
             if (Month <= 6 && !Days.Where(d => d == 31).Any()) Days.Add(31);
+        }
+
+        public enum MonthType
+        {
+            None,
+            Full,
+            Numbers,
+            Letters
         }
     }
 }
