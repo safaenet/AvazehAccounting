db4b4724bfd2c30ef55c0a924898d18a63cf8df7
Safa Seed <safa.dh@gmail.com>
2022-12-30 12:49:32 +0330

    20

---
+++ AvazehWebApi/Program.cs
@@ -0,0 +1,10 @@
+﻿using Serilog;
+using Serilog.Events;
+
+Log.Logger = new LoggerConfiguration()
+                    .MinimumLevel.Debug()
+                    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                    .Enrich.FromLogContext()
+                    .WriteTo.File(@"log\LogFile.txt")
+                    .WriteTo.Console()
+                    .CreateLogger();
